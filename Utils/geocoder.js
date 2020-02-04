ce809430a139d8598dfba73e17554b0773e962df
amandesai01 <amandesai01@gmail.com>
2020-02-04 19:36:14 +0530

    Geocoder Debugged and Tested.

--- Utils/geocoder.js
+++ Utils/geocoder.js
@@ -4,11 +4,12 @@ url = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
 params = ".json?access_token=pk.eyJ1IjoiYW1hbmRlc2FpMDEiLCJhIjoiY2s2N3c1czZwMDZ5YzNqcGdwMWlidndtaiJ9._ZfmHWHgOaWcanGrdMBmTg&limit=1";
 
 const geocoder = (address, callback) => {
-    request(url + address + params, (error, response) => {
+    request({ url: url + address + params, json: true}, (error, response) => {
         if(error || response.body.features.length === 0) {
             callback(true, undefined)
-        } else {
-            callback(undefined, { latitude: response.body.features[0].center[0], longitude: response.body.features[0].center[1]})
+        }
+        else {
+            callback(false, { latitude: response.body.features[0].center[0], longitude: response.body.features[0].center[1]});
         }
     });
 };
