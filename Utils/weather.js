42bfce69994bb4ae873cedd4d34c6f4a130c4037
amandesai01 <amandesai01@gmail.com>
2020-02-04 18:55:38 +0530

    Typo Fix

--- Utils/weather.js
+++ Utils/weather.js
@@ -2,16 +2,16 @@ const request = require('request')
 
 const weather = (latitude, longitude, callback) => {
     url = "https://api.darksky.net/forecast/71df352ffcc6d408df4717c3bdeaa541/" + latitude + "," + longitude
-    request(url, (error, response) => {
+    request({ url: url, json:true}, (error, response) => {
         if(error){
             callback("Some Network Issue", undefined)
-        } else if(response.error.length > 0){
-            callback("Server Side Error : " + response.error, undefined)
+        } else if(response.body.error.length > 0){
+            callback("Server Side Error : " + response.body.error, undefined)
         } else {
             newresponse = {
-                summary : response.currently.summary,
-                temprature : response.currently.temprature,
-                humidity: response.currently.humidity
+                summary : response.body.currently.summary,
+                temprature : response.body.currently.temprature,
+                humidity: response.body.currently.humidity
             };
             callback(undefined, newresponse);
         }
