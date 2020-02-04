19d49089c2aa5817a2d0c6df5baf635f7681e982
amandesai01 <amandesai01@gmail.com>
2020-02-04 19:40:54 +0530

    Weather Module Debugged and Tested.

--- Utils/weather.js
+++ Utils/weather.js
@@ -1,16 +1,16 @@
 const request = require('request')
 
 const weather = (latitude, longitude, callback) => {
-    url = "https://api.darksky.net/forecast/71df352ffcc6d408df4717c3bdeaa541/" + latitude + "," + longitude
+    url = "https://api.darksky.net/forecast/71df352ffcc6d408df4717c3bdeaa541/" + latitude + "," + longitude;
     request({ url: url, json:true}, (error, response) => {
         if(error){
             callback("Some Network Issue", undefined)
-        } else if(response.body.error.length > 0){
+        } else if(response.body.error){
             callback("Server Side Error : " + response.body.error, undefined)
         } else {
             newresponse = {
                 summary : response.body.currently.summary,
-                temprature : response.body.currently.temprature,
+                temprature : response.body.currently.temperature,
                 humidity: response.body.currently.humidity
             };
             callback(undefined, newresponse);
