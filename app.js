dab95cd8537a11ca4feb28bebeb9a02cadc6d0a0
amandesai01 <amandesai01@gmail.com>
2020-02-04 19:41:25 +0530

    Main Interface Debugged and Tested.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const geocoder = require('/Utils/geocoder')
-const weather = require('/Utils/weather')
+const geocoder = require('./Utils/geocoder')
+const weather = require('./Utils/weather')
 
 const address = process.argv[2]
 
 if(!address){
     console.log("Please Provide Address as an Argument")
 } else {
-    geocoder(address, (response, error) => {
+    geocoder(address, (error, response) => {
         if(error){
             console.log("Error while retrieving coordinates");
         } else {
-            weather(response.latitude, response.longitude, (newresponse, iferror) =>{
+            weather(response.latitude, response.longitude, (iferror, response) =>{
                 if(iferror){
                     console.log("Error while retrieving data: " + iferror);
                 } else {
